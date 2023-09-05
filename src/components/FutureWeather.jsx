b23522b3265726371193824d4706cd11cb971cfc
batuncer <tuncerbaki@gmail.com>
2023-09-05 21:00:40 +0100

    API is working now. BUt i have a problem with icons for futureweather

--- src/components/FutureWeather.jsx
+++ src/components/FutureWeather.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Icon from "./Icon/Icon";
 function FutureWeather({ futureWeatherData }) {
   return (
     <div className="future-weather-container">
       <h2>Future Weather</h2>
-      {/* Render future weather data here */}
+      {futureWeatherData.map((forecast, index) => (
+          <div key={index} className="future-weather-item">
+            <div className="forecast-date">{forecast.dt_txt}</div>
+            <div className="forecast-temperature">{forecast.main.temp}°C</div>
+            <div className="forecast-description">{forecast.weather[0].description}</div>
+            <Icon name={forecast.weather[0].main.toLowerCase()} />
+          </div>
+        ))}
     </div>
   );
 }
